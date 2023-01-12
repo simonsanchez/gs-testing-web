689d29493e55ab396a21eef7d24fcf92a89c5636
Simon Sanchez <simonsanchez@github.com>
2023-01-12 14:17:50 -0800

    Add `testng` plugin

--- complete/src/test/java/com/example/testingweb/TestingWebApplicationTest.java
+++ complete/src/test/java/com/example/testingweb/TestingWebApplicationTest.java
@@ -1,28 +1,36 @@
 package com.example.testingweb;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.junit.jupiter.api.Test;
-
+import com.example.testingweb.TestingWebApplication;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-@AutoConfigureMockMvc
-public class TestingWebApplicationTest {
+public class TestingWebApplicationTest extends AbstractTestNGSpringContextTests {
 
 	@Autowired
+	private WebApplicationContext webApplicationContext;
+
 	private MockMvc mockMvc;
 
+	@BeforeClass
+	public void setup() {
+		mockMvc = MockMvcBuilders
+                 .webAppContextSetup(webApplicationContext)
+                 .build();
+	}
+
 	@Test
-	public void shouldReturnDefaultMessage() throws Exception {
-		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-				.andExpect(content().string(containsString("Hello, World")));
+	public void testRootRoute() throws Exception {
+			mockMvc.perform(get("/"))
+							.andExpect(status().isOk());
 	}
 }
